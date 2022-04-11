808af09fb58fc05d3fb03d16c62dc451fecf3083
Alejandro Campos <acamposcar@gmail.com>
2022-04-11 20:37:37 +0200

    Update preview

--- src/components/preview/Preview.js
+++ src/components/preview/Preview.js
@@ -1,6 +1,25 @@
-function Preview() {
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Box from '@mui/material/Box';
+import EducationPreview from './EducationPreview';
+import ExperiencePreview from './ExperiencePreview';
+import PersonalPreview from './PersonalPreview';
+
+function Preview(props) {
+  const { education, personal, experience } = props;
+
   return (
-    <h1>Preview</h1>
+
+    <Box>
+      <h2>Preview</h2>
+      <Card variant="outlined" sx={{ minWidth: '700px', marginBottom: 3, padding: 4 }}>
+        <CardContent>
+          <PersonalPreview personal={personal} />
+          <ExperiencePreview experience={experience} />
+          <EducationPreview education={education} />
+        </CardContent>
+      </Card>
+    </Box>
   );
 }
 export default Preview;
